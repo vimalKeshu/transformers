a7c2be71b5425875da89feb6ea7b197f8f97b222
vimalKeshu <vmal.chaudhari@gmail.com>
2025-01-11 20:38:50 +0530

    learning transformer bidrectional encoder-decoder architecture

---
+++ bidirectional_transformers/config.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 20,
+        "lr": 10**-4,
+        "seq_len": 350,
+        "d_model": 512,
+        "datasource": 'opus_books',
+        "lang_src": "en",
+        "lang_tgt": "it",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return '/'.join([current_directory, model_folder, model_filename])
+
+# Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{current_directory}/{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
