0c909d0dcb68869d6a9f791c2467cf4a557e9539
vimalKeshu <vmal.chaudhari@gmail.com>
2025-01-12 16:17:16 +0530

    updated the code for inference methods.

--- decoder_only_transformers/shakespeare_inference.py
+++ decoder_only_transformers/shakespeare_inference.py
@@ -5,9 +5,10 @@ from shakespeare_config import (get_config,
                                 current_directory)
 import torch 
 import warnings
+import heapq
 from train import build_transformer
 
-def predict(start_str:str, max_predicted_tokens:int=512)-> None:
+def predict_with_greedy_search(start_str:str)-> None:
     config:dict=get_config() 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
@@ -42,12 +43,7 @@ def predict(start_str:str, max_predicted_tokens:int=512)-> None:
     print(f'Model output: {output}')
 
 
-import torch
-import heapq
-from shakespeare_config import get_config, latest_weights_file_path, get_gpt2_tokenizer, causal_mask
-
 def predict_with_beam_search(start_str: str, 
-                             max_predicted_tokens: int = 512, 
                              beam_width: int = 3) -> None:
     config: dict = get_config() 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -66,13 +62,12 @@ def predict_with_beam_search(start_str: str,
 
     # Initial input
     start_tokens = tokenizer.encode(start_str)
-    input = torch.tensor(data=start_tokens, dtype=torch.int64).unsqueeze(dim=0).to(device)
-    mask = causal_mask(input.size(1)).to(device)
+    input = torch.tensor(data=start_tokens, dtype=torch.int64).unsqueeze(dim=0).to(device) # (1, seq_len)
 
     # Beam search variables
     beams = [(0, input, [])]  # Each beam is a tuple of (score, sequence, tokens_generated)
 
-    for _ in range(max_predicted_tokens):
+    for _ in range(config['seq_len']):
         all_candidates = []
 
         # Process each beam
@@ -95,7 +90,7 @@ def predict_with_beam_search(start_str: str,
         beams = heapq.nsmallest(beam_width, all_candidates, key=lambda x: x[0])
 
         # Optionally, stop early if all beams end with an EOS token
-        if all(beam[1][:, -1].item() == tokenizer.eos_token_id for beam in beams):
+        if all(beam[1].shape[1] >= config['seq_len'] for beam in beams):
             break
 
     # Retrieve the best beam (with the highest score)
@@ -106,12 +101,9 @@ def predict_with_beam_search(start_str: str,
     output = tokenizer.decode(best_tokens, skip_special_tokens=True)
     print(f'Model output: {output}')
 
-
-# if __name__ == '__main__':
-#     start_str = 'Now sadder, that you come so'
-#     predict_with_beam_search(start_str=start_str)
-
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     start_str = 'Now sadder, that you come so'
-    predict(start_str=start_str)
+    predict_with_greedy_search(start_str=start_str)
+    print('--'*100)
+    predict_with_beam_search(start_str=start_str)
